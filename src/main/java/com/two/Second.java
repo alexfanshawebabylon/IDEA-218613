057faf52b3c8d796026aa3069000b9e11fc6a189
alexfanshawebabylon <alexander.fanshawe@babylonhealth.com>
2019-07-25 17:49:30 +0100

    add test and config

---
+++ src/main/java/com/two/Second.java
@@ -0,0 +1,8 @@
+package com.two;
+
+public class Second {
+
+    public void two(){
+        System.out.println("two");
+    }
+}
---
+++ src/test/java/com/two/SecondTest.java
@@ -0,0 +1,11 @@
+package com.two;
+
+import org.junit.Test;
+
+public class SecondTest {
+
+    @Test
+    public void name() {
+        new Second().two();
+    }
+}
